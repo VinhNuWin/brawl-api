f66ef4d35084a16847c32dd3cf180706dda5d4a1
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-16 12:41:26 -0800

    added bodyParser and 'next' to CORS

--- index.mjs
+++ index.mjs
@@ -2,12 +2,22 @@ import express from 'express';
 const app = express();
 import brawlers from './brawlers.json' assert{ type: "json"};
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.use(function(req, res) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+        if(req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET' )
+            return res.status(200).json({});
+        }
 });
 
 app.get('/', (req, res) => {
