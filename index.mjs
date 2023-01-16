5eadc6bdf0ac79ccc50351f07691c2b1e310e971
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-16 11:44:25 -0800

    addeed cors res.header

--- index.mjs
+++ index.mjs
@@ -2,9 +2,13 @@ import express from 'express';
 const app = express();
 import brawlers from './brawlers.json' assert{ type: "json"};
 import cors from 'cors';
-app.use(cors({
-    origin: "*",
-}))
+
+app.use(cors());
+
+app.use(function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
 
 app.get('/', (req, res) => {
     res.send(brawlers);
